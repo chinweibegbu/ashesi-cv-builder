36fa565c0078f9183a1dd34dc708a552fee2adbb
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-10-22 22:19:17 +0100

    Added non-responsive landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import '../styles/LandingPage.css';
+import TitleBar from "./TitleBar";
+
+function LandingPage() {
+    return (
+        <>
+            <TitleBar location="landing-page" />
+            <div className="row-flex d-flex m-5 p-5 align-items-center bordered h-75">
+                <div className="col-8 p-5 bordered">
+                    <p className="landing-page-text m-0">
+                        Build an Ashesi-approved CV with ease
+                    </p>
+                    <p className="landing-page-subtext mt-1">
+                        With a bunch more features and a lot less hassle!
+                    </p>
+                    <button className="landing-page-button button btn btn-lg">
+                        Log In
+                    </button>
+                </div>
+                <div className="col-4 d-flex justify-content-center bordered">
+                    <img src="images/landing-page-avatar-darker-tone.png" alt="the company logo" />
+                </div>
+            </div>
+        </>
+    );
+}
+
+
+export default LandingPage;
