8d4e90d3184d619d9c5e41ded0d53d73cb29e2ef
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-10-25 02:40:15 +0100

    Completed SkillEntry UI

--- src/components/CVEditorEntries/SkillEntry.js
+++ src/components/CVEditorEntries/SkillEntry.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { skillLevels } from "../../data/skillLevels";
 
 function SkillEntry({ id, sectionTag, handleEntryAddition }) {
+    const [skillType, setSkillType] = useState(false);
+    const handleSelect = (event) => {
+        setSkillType(event.target.value);
+    }
+
     const makeInvisible = (handleEntryAddition) ? "" : " invisible";
 
     return (
         <div id={id}>
             <form className="bordered row">
                 <div className="form-group col-md-6 d-flex flex-column mb-2">
-                    <label htmlFor={sectionTag + "-first-name"}>First Name</label>
-                    <input type="text" id={sectionTag + "-first-name"} placeholder="e.g. Chinwe"></input>
+                    <label htmlFor={sectionTag + "-name"}>Skill Name</label>
+                    <input type="text" id={sectionTag + "-name"} placeholder="e.g. Python"></input>
                 </div>
-                <div className="form-group col-md-6 d-flex flex-column">
-                    <label htmlFor={sectionTag + "-last-name"}>Last Name</label>
-                    <input type="text" id={sectionTag + "-last-name"} placeholder="e.g. Ibegbu"></input>
+                <div className="form-group col-md-6 d-flex flex-column mb-2">
+                    <label htmlFor={sectionTag + "-skill-level"}>Skill Level</label>
+                    <select className="form-select" id={sectionTag + "-skill-level"} aria-label={skillLevels[0]} defaultValue={skillLevels[0]}>
+                        {
+                            skillLevels.map((skillLevel, key) => {
+                                return <option key={key} value={skillLevel}>{skillLevel}</option>;
+                            })
+                        }
+                    </select>
+                </div>
+                <div className="form-group col-md-6 d-flex flex-column mb-2">
+                    <label htmlFor={sectionTag + "-skill-type"}>Skill Type</label>
+                    <select className="form-select" id={sectionTag + "-skill-type"} aria-label="Hard Skill" defaultValue="Hard Skill" value={skillType} onChange={handleSelect}>
+                        <option value="Hard Skill">Hard Skill</option>
+                        <option value="Soft Skill">Soft Skill</option>
+                    </select>
                 </div>
             </form>
-            <div className={"add-entry d-flex align-content-center mt-1"+makeInvisible} onClick={handleEntryAddition}>
+            <div className={"add-entry d-flex align-content-center mt-1" + makeInvisible} onClick={handleEntryAddition}>
                 <i className="bi-plus-circle me-1" />
                 <p>Add skill</p>
             </div>
---
+++ src/data/skillLevels.js
@@ -0,0 +1,1 @@
+export const skillLevels = ["Novice", "Intermediate", "Advanced", "Proficient", "Expert"];
