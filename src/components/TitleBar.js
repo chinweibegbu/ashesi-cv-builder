4fec3c28a1a7e739e872e3ed1a09d0e564d55c45
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-10-31 05:34:40 +0100

    Added logout functionality

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/TitleBar.css';
 
 function TitleBar({ location }) {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        navigate("/");
+    }
+
     if (location === "landing-page") {
         return (
             <div className="row-flex d-flex title-bar-landing p-2 bordered">
@@ -10,9 +17,14 @@ function TitleBar({ location }) {
         );
     } else if (location === "main-body") {
         return (
-            <div className="row-flex d-flex title-bar-main bordered">
-                <img className='logo-img' src="images/ashesi-logo-white.png" alt="Ashesi University Logo" />
-                <p className='logo-txt text-white'>Ashesi CV Builder</p>
+            <div className="row-flex d-flex title-bar-main align-items-center justify-content-between bordered">
+                <div className="row-flex d-flex align-items-center">
+                    <img className='logo-img' src="images/ashesi-logo-white.png" alt="Ashesi University Logo" />
+                    <p className='logo-txt text-white'>Ashesi CV Builder</p>
+                </div>
+                <div>
+                    <p className='logout-link m-auto text-white text-decoration-underline' onClick={handleLogout}>Log out</p>
+                </div>
             </div>
         );
     }
