96b2402d6be34e90e56db7cce88990feb8865b7a
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-11-01 05:45:50 +0100

    Fixed PDF download scale issue

--- src/components/CVPreview.js
+++ src/components/CVPreview.js
@@ -1,27 +1,32 @@
 import React from "react";
 // import JsPDF from 'jspdf';
-import { ReactToPdf, usePDF } from 'react-to-pdf';
+import generatePDF, { Margin } from 'react-to-pdf';
 import '../styles/CVPreview.css';
 import CVpdf from "./CVpdf.js";
 
 function CVPreview({ cvDetails }) {
     const downloadName = cvDetails.cvName + ".pdf";
-    // const generatePDFWithJsPDF = () => {
-    //     const report = new JsPDF('portrait', 'pt', 'a4');
-    //     report.html(document.querySelector('#cv-pdf-reference')).then(() => {
-    //         report.save(downloadName);
-    //     });
-    // }
+    const targetRef = React.useRef();
 
-    // Used react-to-pdf >> ISSUE: Size/scale
-    const { toPDF, targetRef } = usePDF({ filename: downloadName });
+    const options = {
+        filename: downloadName,
+        page: {
+            margin: Margin.SMALL
+        },
+        overrides: {
+            // See https://html2canvas.hertzen.com/configuration for more options
+            canvas: {
+                scale: 5
+            }
+        },
+    };
 
     return (
         <div className="cv-editor-preview d-flex flex-column align-items-center col-xl-6 bordered">
             <p className="title">CV Preview</p>
             <CVpdf cvPreviewDetails={cvDetails} targetRef={targetRef} />
             <div className="interaction-btns mt-2">
-                <button className="button btn mx-1" onClick={() => toPDF()}>Download CV</button>
+                <button className="button btn mx-1" onClick={() => generatePDF(targetRef, options)}>Download CV</button>
                 <button className="button-icon btn mx-1">
                     <i className="bi-share" />
                 </button>
