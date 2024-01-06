0f2b551b8327675a0045059fa80f0d1e383f27b7
chinweibegbu <chinwe.ibegbu@gmail.com>
2024-01-06 17:22:18 +0100

    Reformatted createUser controller function

--- src/api/controllers/user-controller.js
+++ src/api/controllers/user-controller.js
@@ -39,18 +39,22 @@ export const createUser = (req, res) => {
                     userQueries.createUserQuery,
                     [newUser.full_name, newUser.email, newUser.password],
                     (error, results) => {
-                        if (error) throw error;
-                        // Get and return created user 
-                        pool.query(
-                            userQueries.checkIfEmailExistsQuery,
-                            [newUser.email],
-                            (error, results) => {
-                                if (error) throw error
-                                if (results.length === 1) {
-                                    res.status(201).send(results.rows[0]);
+                        if (error) {
+                            throw error;
+                        } else {
+                            console.log(results);
+                            // Get and return created user 
+                            pool.query(
+                                userQueries.checkIfEmailExistsQuery,
+                                [newUser.email],
+                                (error, results) => {
+                                    if (error) throw error
+                                    if (results.length === 1) {
+                                        res.status(201).send(results.rows[0]);
+                                    }
                                 }
-                            }
-                        );
+                            );
+                        }
                     }
                 );
             }
